a783022aa66119e214ed6c3c7824492a37758793
apshu <attila.kolinger@gmail.com>
2020-06-05 15:35:08 +0200

    HersheyFonts-#4 Fix 'Redefining built-in &#39;open&#39;' issue in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
-from io import open
+import io
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with io.open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
